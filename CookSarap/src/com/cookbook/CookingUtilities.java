ccab95b2e2241204e4bf21d0450ac05769bf1865
theaicon <irenebandilla@gmail.com>
2013-03-21 05:25:19 +0800

    utensils added

--- CookSarap/src/com/cookbook/CookingUtilities.java
+++ CookSarap/src/com/cookbook/CookingUtilities.java
@@ -2,6 +2,8 @@ package com.cookbook;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.GridView;
 
 public class CookingUtilities extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +11,15 @@ public class CookingUtilities extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cookinguten);
+        
+        GridView gridview = (GridView) findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(this));
+
+        //gridview.setOnItemClickListener(new OnItemClickListener() {
+          //  public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+            //    Toast.makeText(HelloGridView.this, "" + position, Toast.LENGTH_SHORT).show();
+          //  }
+        //});
     }
+       
 }
